35e0d70aa2d7bf722e473af18afe5a411d689bec
heckergorl <chezan.carmen@gmail.com>
2022-12-06 11:15:13 +0100

    ik simp voor ......

--- docker_desktop_gebeuren/run.sh
+++ docker_desktop_gebeuren/run.sh
@@ -3,6 +3,6 @@
 #   --class org.apache.spark.deploy.SparkSubmit \
 #   spark-3.3.1-bin-hadoop3/examples/src/main/python/pi.py
 
-./spark-3.3.1-bin-hadoop3/bin/spark-submit \
+spark-submit \
   --master spark://172.20.0.1:7077 \
   spark-3.3.1-bin-hadoop3/examples/src/main/python/pi.py
